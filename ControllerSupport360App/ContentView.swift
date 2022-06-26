4b1c687e9a6a1105cea7ce84b318657bf1079512
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2022-06-25 20:59:53 -0400

    Initial Commit

---
+++ ControllerSupport360App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ControllerSupport360App
+//
+//  Created by Skyler Calaman on 6/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ControllerSupport360App/ControllerSupport360AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControllerSupport360AppApp.swift
+//  ControllerSupport360App
+//
+//  Created by Skyler Calaman on 6/25/22.
+//
+
+import SwiftUI
+
+@main
+struct ControllerSupport360AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
